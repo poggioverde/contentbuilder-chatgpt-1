568c8311070b889acab1b14cac79e27c8c0ac9f3
psapir <patriciosapir@gmail.com>
2023-04-06 17:57:34 -0400

    updates to code.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+var bodyParser  = require('body-parser');
 const app = express()
 const port = 3000
 const chatgpt = require('./routes/chatgpt');
 
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({ type: 'application/json', limit: '50mb', extended: true }));
 // routes
 app.post('/chatgpt/getResults/',chatgpt.getResults);
 
